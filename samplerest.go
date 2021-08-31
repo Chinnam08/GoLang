053ef33eb29dfd0284997b16965725fbe51d8fed
Chinnam08 <86693330+Chinnam08@users.noreply.github.com>
2021-08-31 10:59:47 -0500

    Initial Commit

---
+++ samplerest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", handler)
+	route.HandleFunc("/users", getUsers)
+	//db.GetDB()
+	fmt.Println("Starting the server")
+	http.ListenAndServe(":8080", route)
+}
+
+func handler(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Request Landed..!")
+	fmt.Fprintf(resp, "Welcome to the HomePage!")
+}
+
+func getUsers(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Request Landed for Get users..!")
+	// TODO
+}
